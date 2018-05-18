3b828fceb6454ce2370e5d7e37cf1947e7b61411
NathanPB <nathan.pbombana@gmail.com>
2018-05-17 23:59:56 -0300

    Event Bus is done

--- src/main/kotlin/cf/nathanpb/dogo/core/eventBus/EventBus.kt
+++ src/main/kotlin/cf/nathanpb/dogo/core/eventBus/EventBus.kt
@@ -2,31 +2,67 @@ package cf.nathanpb.dogo.core.eventBus
 
 import cf.nathanpb.dogo.core.queue.DogoQueue
 import java.util.*
+import kotlin.collections.ArrayList
 import kotlin.collections.LinkedHashMap
 import kotlin.reflect.KFunction
-import kotlin.reflect.KTypeParameter
+import kotlin.reflect.full.createType
+import kotlin.reflect.full.functions
 
 class EventBus(name : String) : DogoQueue(name){
     private val listeners = LinkedHashMap<Long, EventBus.EventListener>()
 
-
-
     fun submit(element : Any) {
         submit {
-            for(listener in listeners){
-                //todo continuar daqui
+            submitSink(element)
+        }
+    }
+
+    fun submitSink(element : Any) {
+        for(listener in listeners){
+            val parameter = listener.value.func.parameters.first()
+            println(listener.value.func.parameters.map { p -> p.type})
+
+            if(parameter.type.equals(element::class.createType())){
+                listener.value.func.call(listener.value.instance, element)
             }
         }
+    }
+
+    fun register(element: Any) : List<Long> {
+        val funcs = element::class.functions
+                .filter { m -> m.parameters.size == 2 }
+                .filter { m -> m.annotations.any { a -> a.annotationClass.equals(EventBus.Listener::class) } }
+        val ids = ArrayList<Long>()
+        for(func in funcs){
+            val el = EventBus.EventListener(func, this, element)
+            listeners[el.id] = el
+            ids.add(el.id)
+        }
+        return ids
+    }
+
+    fun unregister(vararg ids : Long) {
+        for(id in ids){
+            listeners.remove(id)
+        }
+    }
+
+    fun unregister(instance : Any) {
+        LinkedHashMap<Long, EventBus.EventListener>(listeners)
+                .forEach {
+                    t, u -> if(u.instance.equals(instance))  unregister(t)
+                }
     }
 
     @Retention(AnnotationRetention.RUNTIME)
     @Target(AnnotationTarget.FUNCTION)
     annotation class Listener(val value : Int)
 
-    private class EventListener (func : KFunction<Any>, bus : EventBus) {
+    private class EventListener constructor( func : KFunction<Any?>, bus : EventBus, instance : Any) {
         val func = func
         var id = 0L
         var priotity = 0
+        val instance = instance
 
         init {
             do {
@@ -36,6 +72,10 @@ class EventBus(name : String) : DogoQueue(name){
             if(func.annotations.filter { a -> a.annotationClass.equals(Listener::class) }.isNotEmpty()){
                 priotity = (func.annotations.filter { a -> a.annotationClass.equals(Listener::class) }.first() as Listener).value
             }
+        }
+
+        override fun toString(): String {
+            return "EventListener@"+id
         }
     }
 }
