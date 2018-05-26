ac5cf2bba0ab0c0b9f57d03723067a3fd3bfbe81
NathanPB <nathan.pbombana@gmail.com>
2018-05-26 20:59:30 -0300

    DogoQueue is the new DogoThread

--- src/main/kotlin/cf/nathanpb/dogo/core/queue/DogoQueue.kt
+++ src/main/kotlin/cf/nathanpb/dogo/core/queue/DogoQueue.kt
@@ -1,22 +1,51 @@
 package cf.nathanpb.dogo.core.queue
 
+import cf.nathanpb.dogo.core.DogoBot
+import java.util.*
 import java.util.concurrent.LinkedBlockingQueue
 
-open class DogoQueue(name : String) : DogoThread(name, {}) {
+
+open class DogoQueue(name : String) : TimerTask() {
     private val queue = LinkedBlockingQueue<QueueAction>()
-    init {
-        this.run = {
-            if(!queue.isEmpty()){
-                queue.poll().run()
-            }
+    private var lastTick01 = System.currentTimeMillis()
+    private var lastTick02 = System.currentTimeMillis()
+    public val name = name
+    open var run = {
+        if(!queue.isEmpty()){
+            queue.poll().run()
         }
     }
+    private var timer = Timer(name)
+    var minClock = 100
+
+    init {
+        DogoBot.threads.put(name, this)
+    }
+
+    override fun run() {
+        kotlin.run(run)
+        lastTick02 = lastTick01
+        lastTick01 = System.currentTimeMillis()
+    }
+
+    fun getTps() : Int {
+        if(lastTick01.equals(lastTick02)) {
+            return 0
+        }
+        return 1000 / (lastTick01 - lastTick02).toInt()
+    }
+
+    open fun shedule(period : Long) {
+        timer.schedule(this, 0, period-1)
+    }
+
+
 
     open fun submit(action : () -> Unit) {
         queue.add(QueueAction(action))
     }
 
-    fun size() : Int {
+    fun queue() : Int {
         return queue.size
     }
 
