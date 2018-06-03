f734673ee07e77670d8a43253c5c9e7830f05e41
NathanPB <nathan.pbombana@gmail.com>
2018-06-02 23:16:04 -0300

    Bug Fix

--- src/main/kotlin/cf/nathanpb/dogo/interfaces/IRepliable.kt
+++ src/main/kotlin/cf/nathanpb/dogo/interfaces/IRepliable.kt
@@ -14,22 +14,19 @@ interface IRepliable {
 
     fun reply(vararg content : Any, preset : Boolean = false){
         DogoBot.jdaOutputThread.submit {
-            replySynk(content, preset)
+            replySynk(*content, preset)
         }
     }
 
     fun replySynk(vararg content : Any, preset : Boolean = false) : Message {
-        var text = content[0].toString()
-        if(preset){
-            langEntry().getText(lang(), text)
-        } else {
-            if(content[0] is String) {
-                text = String.format(text, Arrays.copyOfRange(content, 1, content.size - 1))
-            }
-        }
         if(content[0] is MessageEmbed) {
             return replyChannel().sendMessage(content[0] as MessageEmbed).complete()
         } else {
+            var text = content[0].toString()
+            if(preset){
+                text = langEntry().getText(lang(), text)
+            }
+                String.format(text, *Arrays.copyOfRange(content, 1, content.size - 1))
             return replyChannel().sendMessage(text).complete()
         }
     }
