6f57c89bcb7f09bdc8d85c25b9920cafc047eaa4
NathanPB <nathan.pbombana@gmail.com>
2018-06-22 16:16:57 -0300

    Logger is now a PrintStream

--- src/main/kotlin/cf/dogo/core/Logger.kt
+++ src/main/kotlin/cf/dogo/core/Logger.kt
@@ -1,16 +1,17 @@
 package cf.dogo.core
 
 import cf.dogo.core.queue.DogoQueue
+import cf.dogo.utils.ConsoleColors
 import java.io.File
 import java.io.FileWriter
+import java.io.OutputStream
 import java.io.PrintStream
 import java.text.SimpleDateFormat
 import java.util.*
 
-class Logger constructor(ps : PrintStream, name : String){
+class Logger constructor(os : OutputStream?, name : String) : PrintStream(os) {
     val format = SimpleDateFormat("HH:mm:ss - dd/MM/YYYY")
     private val name = name
-    private val ps  = ps;
     private val file = File(File(cf.dogo.core.DogoBot.data?.getString("LOG_PATH")), "$name _${format.format(Date()).replace("/", "-").replace(":", "-")}.log")
     val fwqueue = DogoQueue("LOGGER_FW_QUEUE")
     init {
@@ -24,38 +25,32 @@ class Logger constructor(ps : PrintStream, name : String){
     val fw = FileWriter(file, true)
 
 
-    fun print(obj : Any?) {
-        val s = obj.toString()+cf.dogo.utils.ConsoleColors.RESET.toString()
-        ps.print(s)
-        printToFile(s)
+
+    fun printlnRaw(obj : Any?){
+        print("$obj \n")
     }
 
-    fun println(obj : Any?){
-        val s = obj.toString()+cf.dogo.utils.ConsoleColors.RESET.toString()+"\n"
-        print(s)
+    override fun print(obj : Any?) {
+        super.println(obj)
+        printToFile(obj.toString())
     }
 
-    fun info(obj : Any?, color : cf.dogo.utils.ConsoleColors) {
-        println("$color["+format.format(Date())+"] [INFO] [${Thread.currentThread().name.toUpperCase()}] "+obj)
+    override fun println(obj : Any?) {
+        info(obj)
     }
 
-    fun info(obj : Any?) {
-        info(obj, cf.dogo.utils.ConsoleColors.RESET)
+    fun info(obj : Any?, color : cf.dogo.utils.ConsoleColors = ConsoleColors.RESET, newline : Boolean = true) {
+        var content = "$color[${format.format(Date())}] [INFO] [${Thread.currentThread().name.toUpperCase()}] $obj ${ConsoleColors.RESET}"
+        if(newline) content+="\n"
+        print(content)
     }
-
 
     fun warn(obj : Any?){
-        println(cf.dogo.utils.ConsoleColors.YELLOW.toString()+"["+format.format(Date())+"] [WARN] [${Thread.currentThread().name.toUpperCase()}] "+obj.toString())
+        print("${ConsoleColors.YELLOW}[${format.format(Date())}] [WARN] [${Thread.currentThread().name.toUpperCase()}] $obj ${ConsoleColors.RESET} \n")
     }
-
     fun error(obj : Any?){
-        println(cf.dogo.utils.ConsoleColors.BLACK_BOLD.toString()+cf.dogo.utils.ConsoleColors.RED_BACKGROUND.toString()+"["+format.format(Date())+"] [ERROR] [${Thread.currentThread().name.toUpperCase()}] "+obj.toString())
+        print("${ConsoleColors.BLACK_BOLD} ${ConsoleColors.RED_BACKGROUND}[${format.format(Date())}] [WARN] [${Thread.currentThread().name.toUpperCase()}] $obj ${ConsoleColors.RESET} \n")
     }
-
-    fun getPrintStream() : PrintStream {
-        return ps
-    }
-
 
     private fun printToFile(string: String){
         fwqueue.submit {
