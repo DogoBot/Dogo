0b1f5463f08c36a99c4c2dd359900344c9018797
Nathan Bombana <nathan.pbombana@gmail.com>
2018-12-11 02:05:16 -0200

    Documentation o/

--- src/main/kotlin/io/github/dogo/badwords/BadwordListener.kt
+++ src/main/kotlin/io/github/dogo/badwords/BadwordListener.kt
@@ -8,11 +8,43 @@ import net.dv8tion.jda.core.entities.Message
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent
 import java.awt.Color
 
+/*
+Copyright 2019 Nathan Bombana
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/**
+ * Simple class to listen the [GuildMessageReceivedEvent] from JDA and check its messages.
+ *
+ * @author NathanPB
+ * @since 3.1.0
+ */
 class BadwordListener {
 
+    /**
+     * Listens the events.
+     *
+     * @param[e] the event, sent from [EventBus]
+     */
     @EventBus.Listener
     fun listenSend(e: GuildMessageReceivedEvent) = check(e.message)
 
+    /**
+     * Checks if the guild's message has a badword and process it.
+     *
+     * @param[msg] the message.
+     */
     fun check(msg: Message) {
         DogoGuild(msg.guild).let { guild ->
             val bw = guild.badwords.badwords
@@ -26,9 +58,8 @@ class BadwordListener {
                                 for(i in 0..word.length) sw.append("*")
                                 sw.toString()+"``"
                             } else word
-                        }
-                        .joinToString(" ")
-                if (!newmsg.equals(msg.contentDisplay)) {
+                        }.joinToString(" ")
+                if (newmsg != msg.contentDisplay) {
                     msg.channel.sendMessage(
                             EmbedBuilder()
                                     .setAuthor("${user.formatName()} said", null, user.usr?.effectiveAvatarUrl.orEmpty())
--- src/main/kotlin/io/github/dogo/badwords/BadwordProfile.kt
+++ src/main/kotlin/io/github/dogo/badwords/BadwordProfile.kt
@@ -3,41 +3,99 @@ package io.github.dogo.badwords
 import com.mongodb.client.MongoCollection
 import io.github.dogo.core.DogoBot
 import io.github.dogo.core.entities.DogoGuild
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent
 import org.bson.Document
 
+/*
+Copyright 2019 Nathan Bombana
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/**
+ * Data Class that holds Badwords from guilds.
+ *
+ * @param[guild] the guild.
+ * @param[badwords] the badwords.
+ *
+ * @author NathanPB
+ * @since 3.1.0
+ */
 data class BadwordProfile(val guild: DogoGuild, val badwords: MutableList<String>) {
 
     companion object {
         val col = DogoBot.db?.getCollection("badwords") as MongoCollection
-        val listener = io.github.dogo.badwords.BadwordListener()
+        val listener = BadwordListener()
 
-        private val cache = mutableMapOf<String, io.github.dogo.badwords.BadwordProfile>()
+        /**
+         * Every single guild instance has a cached data once its [BadwordProfile] is accessed. The cache is stored here.
+         */
+        private val cache = mutableMapOf<String, BadwordProfile>()
 
-        fun parse(doc: Document) : io.github.dogo.badwords.BadwordProfile? {
+        /**
+         * Parses a single [Document] into a [BadwordProfile] (nullable)
+         * Required Fields:
+         * -guild: [String]
+         * -badwords: [List]<[String]>
+         *
+         * @param[doc] the document to parse.
+         * @return the [BadwordProfile] (nullable).
+         */
+        fun parse(doc: Document) : BadwordProfile? {
             return try {
-                io.github.dogo.badwords.BadwordProfile(DogoGuild(doc.getString("guild")), (doc["badwords"] as List<String>).toMutableList())
-            }catch (ex: Exception) { null }
+               BadwordProfile(DogoGuild(doc.getString("guild")), (doc["badwords"] as List<String>).toMutableList())
+            } catch (ex: Exception) { null }
         }
 
-        fun clearCache() = io.github.dogo.badwords.BadwordProfile.Companion.cache.clear()
-        fun cachedEntry(guild: DogoGuild) = io.github.dogo.badwords.BadwordProfile.Companion.cache[guild.id] ?: io.github.dogo.badwords.BadwordFinder().also{it.guild = guild}.find() ?: io.github.dogo.badwords.BadwordProfile(guild, mutableListOf()).also { it.update() }
+        /**
+         * Clears the badwords cache ([cache])
+         */
+        fun clearCache() = BadwordProfile.cache.clear()
+
+        /**
+         * Gets a cached entry for a guild. If its not found, automatically creates a blank [BadwordProfile] and stores it on database (also automatically cached).
+         *
+         * @param[guild] the guild to get the profile.
+         *
+         * @return the profile for the supplied guild.
+         */
+        fun cachedEntry(guild: DogoGuild) = BadwordProfile.cache[guild.id] ?: BadwordFinder().also{it.guild = guild}.find() ?: BadwordProfile(guild, mutableListOf()).also { it.update() }
     }
 
+    /**
+     * Inserts the current profile into database (updates if it already exists).
+     * Also automatically cache the profile (or update the cache if it already exists)
+     */
     fun update(){
-        if(io.github.dogo.badwords.BadwordFinder().also{ it.guild = guild}.count() >= 1) {
-            io.github.dogo.badwords.BadwordProfile.Companion.col.updateOne(Document().append("guild", guild.id), Document("\$set", export()))
+        if(BadwordFinder().also{ it.guild = guild}.count() >= 1) {
+            BadwordProfile.col.updateOne(Document().append("guild", guild.id), Document("\$set", export()))
         } else {
-            io.github.dogo.badwords.BadwordProfile.Companion.col.insertOne(export())
+            BadwordProfile.col.insertOne(export())
         }
-        if(io.github.dogo.badwords.BadwordProfile.Companion.cache.containsKey(guild.id)) io.github.dogo.badwords.BadwordProfile.Companion.cache.remove(guild.id)
-        io.github.dogo.badwords.BadwordProfile.Companion.cache[guild.id] = this
+        if(BadwordProfile.cache.containsKey(guild.id)) BadwordProfile.cache.remove(guild.id)
+        BadwordProfile.cache[guild.id] = this
     }
 
+    /**
+     * Removes the current profile from database. Also remove from cache.
+     */
     fun remove(){
-        io.github.dogo.badwords.BadwordProfile.Companion.col.deleteOne(Document().append("guild", guild.id))
+        col.deleteOne(Document().append("guild", guild.id))
+        if(BadwordProfile.cache.containsKey(guild.id)) BadwordProfile.cache.remove(guild.id)
     }
 
-    fun export() : Document {
-        return Document().append("guild", guild.id).append("badwords", badwords)
-    }
+    /**
+     * Exports the current profile for a [Document] instance, ready to be inserted into MongoDB.
+     */
+    fun export() = Document().append("guild", guild.id).append("badwords", badwords)
 }
