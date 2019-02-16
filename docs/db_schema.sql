ff56f32f2a2d00bc9aea0d5363f09ee1e306e509
Nathata <nathan.pbombana@gmail.com>
2019-02-16 02:30:56 -0200

    Updating DB docs

--- docs/db_schema.sql
+++ docs/db_schema.sql
@@ -7,7 +7,7 @@ create table Users (
 
 create table Guilds (
   id varchar(32) not null,
-  dafault_hook varchar(32),
+  default_hook varchar(32),
 
   primary key(id)
 );
@@ -23,7 +23,7 @@ create table Badwords (
   id int not null auto_increment,
   word varchar(256) not null,
   guild varchar(32) not null,
-  active bit(1) not null default 1,
+  active tinyint(1) not null default 1,
 
   primary key(id),
   foreign key(guild) references Guilds(id)
@@ -56,42 +56,21 @@ create table TicTacToeStatistics (
 create table TTTPlayers (
   statistic int not null,
   duser varchar(32) not null,
-  slot bit(1) not null default 0,
-  winner bit(1) not null default 1,
+  slot tinyint(1) not null default 0,
+  winner tinyint(1) not null default 1,
 
   foreign key(statistic) references TicTacToeStatistics(id),
   foreign key(duser) references Users(id)
 );
 
-create table PermGroups (
-  id int not null auto_increment,
-
-  primary key(id)
-);
-
 create table Permissions (
-  permgroup int not null,
+  guild varchar(32),
+  role varchar(32),
   permission varchar(128) not null,
-  type bit(1) not null default 0,
+  type tinyint(1) not null default 0,
+  is_default tinyint(1) not null default 0,
 
-  foreign key(permgroup) references PermGroups(id)
-);
-
-create table UserBoundPermGroup (
-  id int not null,
-  duser varchar(32) not null,
-
-  primary key(id),
-  foreign key(id) references PermGroups(id),
-  foreign key(duser) references Users(id)
-);
-
-create table RoleBoundPermGroup (
-  id int not null,
-  role varchar(32) not null,
-
-  primary key(id),
-  foreign key(id) references PermGroups(id)
+  foreign key(guild) references Guilds(id)
 );
 
 create table Tokens (
