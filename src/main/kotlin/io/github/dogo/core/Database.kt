b23d98e077a34d30c3899de1cb86c5bbfea948b9
Nathata <nathan.pbombana@gmail.com>
2019-02-16 23:37:32 -0300

    Fixed shit about case sensitive

--- src/main/kotlin/io/github/dogo/core/Database.kt
+++ src/main/kotlin/io/github/dogo/core/Database.kt
@@ -26,58 +26,58 @@ limitations under the License.
  * @since 1.0.0
  */
 class Database {
-    object USERS: Table() {
+    object USERS: Table("Users") {
         val id = varchar("id", 32).primaryKey()
         val lang = varchar("lang", 5).default("en_US")
     }
-    object GUILDS: Table() {
+    object GUILDS: Table("Guilds") {
         val id = varchar("id", 32).primaryKey()
         val defaultHook = varchar("default_hook", 32).nullable()
     }
-    object LOCALPREFIXES: Table() {
+    object LOCALPREFIXES: Table("LocalPrefixes") {
         val prefix = varchar("prefix", 32)
         val guild = varchar("guild", 32) references GUILDS.id
     }
-    object BADWORDS: Table() {
+    object BADWORDS: Table("Badwords") {
         val id = integer("id").autoIncrement().primaryKey()
         val word = varchar("word", 256)
         val guild = varchar("guild", 32) references GUILDS.id
         val active = bool("active").default(true)
     }
-    object BADWORDPUNISHMENT: Table() {
+    object BADWORDPUNISHMENT: Table("BadwordPunishment") {
         val badword = integer("id") references BADWORDS.id
         val user = varchar("duser", 32) references USERS.id
         val date = datetime("date").default(DateTime.now())
     }
-    object STATISTICS: Table() {
+    object STATISTICS: Table("Statistics") {
         val id = integer("id").autoIncrement().primaryKey()
         val date = datetime("date")
     }
-    object TICTACTOESTATISTICS: Table() {
+    object TICTACTOESTATISTICS: Table("TicTacToeStatistics") {
         val id = integer("id").primaryKey() references STATISTICS.id
         val table = varchar("thetable", 9)
     }
-    object TTTPlayers: Table() {
+    object TTTPlayers: Table("TTTPlayers") {
         val statistic = integer("statistic") references TICTACTOESTATISTICS.id
         val user = varchar("duser", 32) references USERS.id
         val slot = bool("slot").default(false)
         val winner = bool("winner").default(true)
     }
-    object PERMISSIONS: Table() {
+    object PERMISSIONS: Table("Permissions") {
         val guild = varchar("guild", 32).references(GUILDS.id).primaryKey().nullable()
         val role = varchar("role", 32).primaryKey().nullable()
         val permission = varchar("permission", 128)
         val type =  bool("type").default(false)
         val isDefault = bool("is_default").default(false)
     }
-    object TOKENS: Table() {
+    object TOKENS: Table("Tokens") {
         val token = varchar("token", 256).primaryKey()
         val user = varchar("duser", 32) references USERS.id
         val type = varchar("type", 16)
         val expiresIn = datetime("expires_in")
         val authTime = date("auth_time")
     }
-    object TOKENSCOPES: Table() {
+    object TOKENCOPES: Table("TokenScopes") {
         val token = varchar("token", 256) references TOKENS.token
         val scope = varchar("scope", 32)
     }
